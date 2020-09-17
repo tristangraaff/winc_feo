0b4f6af3cbd892696d849ea465e588531f826c86
Tristan Graaff <tristangraaff@yahoo.com>
2020-09-17 16:58:04 +0200

    Level 18 eerste opdracht

---
+++ level_018/basic_arrow_functions/script.js
@@ -0,0 +1,14 @@
+// zet de functie om naar een arrow functie
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+ikRockArrowFunctions()
+
+const fivePlusSeven = () => 5 + 7;
+fivePlusSeven();
+
+() => 1 +2;
+
+let myFunction = (a, b) => a + b;
+
+let addFive = a => a + 5;
+
+const createObject =  ({greeting: "hoi"});
