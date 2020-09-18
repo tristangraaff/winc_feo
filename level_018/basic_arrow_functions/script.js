50bbfcf37d79fe0bc39a4248d862e7e66c9d794d
Tristan Graaff <tristangraaff@yahoo.com>
2020-09-18 11:02:15 +0200

    Comment

--- level_018/basic_arrow_functions/script.js
+++ level_018/basic_arrow_functions/script.js
@@ -1,14 +1,17 @@
 // zet de functie om naar een arrow functie
-const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
-ikRockArrowFunctions()
+const ikRockArrowFunctions = () =>
+  console.log("Joe, ik rock de arrow functions!");
+ikRockArrowFunctions();
 
 const fivePlusSeven = () => 5 + 7;
 fivePlusSeven();
 
-() => 1 +2;
+() => 1 + 2;
 
 let myFunction = (a, b) => a + b;
 
-let addFive = a => a + 5;
+let addFive = (a) => a + 5;
 
-const createObject =  ({greeting: "hoi"});
+const createObject = { greeting: "hoi" };
+
+//hoi
