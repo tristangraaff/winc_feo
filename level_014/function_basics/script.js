97bcfa889c64a4fb9cf76a4ffa4f6e5bdb57c05c
tristangraaff <tristangraaff@yahoo.com>
2021-03-16 12:00:03 +0100

    function basics

---
+++ level_014/function_basics/script.js
@@ -0,0 +1,15 @@
+const makePizza = () => {
+  console.log("Take dough");
+  console.log("Add tomato sauce");
+  console.log("Add toppings");
+}
+
+const makeSushi = () => {
+  console.log("make it");
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
